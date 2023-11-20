016e261639b3424bde0186bba351149a3e7b6f87
Dhjali <barbara.vanni@laplateforme.io>
2023-11-20 16:35:14 +0100

    aller plus loin

--- jour01/job05/main.py
+++ jour01/job05/main.py
@@ -1,5 +1,39 @@
+print("--------------JOB05 DE BASE--------------")
+
 alpha=['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','x','y','z']
 
 alpha.reverse()
 
 print (*alpha)
+
+
+# EXO + : sans utiliser reverse 
+
+print("--------------SANS UTILISER REVERSE--------------")
+
+alpha=['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','x','y','z']
+
+index = len(alpha) - 1
+newList = []
+
+while index >= 0:
+    newList.append(alpha[index])
+    index = index - 1
+
+print(*newList)
+
+
+
+# EXO ++ : mélanger mon alphabet puis le retrier 
+#bubble sort 
+#fonction.random
+# commment 
+
+print("--------------RANDOMISER + RANGER--------------")
+
+import random
+
+alpha=['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','x','y','z']
+random.shuffle(alpha)
+print(*alpha)
+
