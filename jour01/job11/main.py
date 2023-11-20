87219209f4250ebb5ccadfa538d71dcc8cba4ab8
Dhjali <barbara.vanni@laplateforme.io>
2023-11-20 12:00:51 +0100

    finish

---
+++ jour01/job11/main.py
@@ -0,0 +1,7 @@
+caractere = "e" 
+phrase = (input("Ecrivez vos pensés : "))
+
+if caractere in phrase: 
+	print ("La lettre -e- se trouve dans la chaîne !")
+else :
+	print ("La chaîne ne contient pas la lettre -e-!")
