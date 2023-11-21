3fd2af325bb0e729203a6f022232c9a39f83a89f
Dhjali <barbara.vanni@laplateforme.io>
2023-11-21 14:53:47 +0100

    job04

--- jour02/job04/main.py
+++ jour02/job04/main.py
@@ -1,12 +1,13 @@
-
-
+        
 while True:
-    n = int(input("Tapez la valeur de n : "))
-    
-    if n > 0:
-        print("La table de multiplication de", n, "est :")
-        for i in range(1, 11): 
-            print(i, "x", n, "=", i * n)
-        break
-    else:
-        print("La valeur de n doit être supérieure à 0. Veuillez réessayer.")
+
+        N = int(input("Tapez la valeur de N : "))
+        if N > 0:
+            for i in range(1, N + 1):
+                print("Table de multiplication de", i,":")
+                for j in range(1, 11):
+                    result = i * j
+                    print(f"{i} x {j} = {result}")
+            break
+        else:
+            print("La valeur de N doit être supérieure à 0. Veuillez réessayer.")
