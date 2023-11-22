46c4ca2f87e0743c7206b497addb71202d94dfe7
Dhjali <barbara.vanni@laplateforme.io>
2023-11-22 11:08:08 +0100

    job06

---
+++ jour03/job07/main.py
@@ -0,0 +1,19 @@
+
+
+def program(langage):
+    langage = langage.lower()
+    if langage == "javascript":
+        print ("tu es un développeur web")
+    elif langage == "python" :
+        print ("tu es un développeur IA")        
+    elif langage == "java" :
+        print ("tu es un développeur logiciel")
+    else :
+        print ("un jour, je serai le meilleur développeur...")
+        
+program("JavaScript")
+program("JAVAscriPT")
+program("java")
+program("python")
+program("pithon")
+        
