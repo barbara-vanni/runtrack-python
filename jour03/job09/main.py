d8e556487204c975a67af2fdc2c9c9f86f20f1c0
Dhjali <barbara.vanni@laplateforme.io>
2023-11-22 16:03:51 +0100

    job09 jour02 update

--- jour03/job09/main.py
+++ jour03/job09/main.py
@@ -1,11 +1,13 @@
 def moyenne(note1, note2, note3):
-    return (note1 + note2 + note3) / 3
+    calcul = round((note1 + note2 + note3) / 3)
+    return (calcul)
 
-note1 = int(input("Note 1 : "))
-note2 = int(input("Note 2 : "))
-note3 = int(input("Note 3 : "))
+note1 = float(input("Note 1 : "))
+note2 = float(input("Note 2 : "))
+note3 = float(input("Note 3 : "))
 
 moyenne_etudiant = moyenne(note1, note2, note3)
+print(moyenne_etudiant)
 
 if moyenne_etudiant >= 15 and moyenne_etudiant <= 20:
     print("Très bon élève")
@@ -15,5 +17,7 @@ elif moyenne_etudiant >= 8 and moyenne_etudiant <= 10:
     print("Élève moyen")
 elif moyenne_etudiant >= 0 and moyenne_etudiant <= 7:
     print("Élève devant faire des efforts")
+elif moyenne_etudiant == 42:
+    print("Mon appartement sent bon l'acajou")
 else:
     print("Moyenne invalide")
