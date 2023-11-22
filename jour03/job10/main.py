47658e54066f830e4ed533b71cc3e1957d347052
Dhjali <barbara.vanni@laplateforme.io>
2023-11-22 11:48:57 +0100

    job10

---
+++ jour03/job10/main.py
@@ -0,0 +1,8 @@
+def pairing():
+    nombre = int(input(" Entrez un nombre entier naturel :"))
+    if nombre % 2 == 0 :
+        print("Vous avez entré un nombre pair")
+    else :
+        print("Vous avez entré un nombre impair")
+
+pairing()
